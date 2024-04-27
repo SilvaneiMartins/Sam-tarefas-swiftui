c417a428d6c98fd57123c3d111ea0e6f3b2a21ee
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-04-27 10:42:50 -0400

    Initial Commit

---
+++ SamNotes/SamNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamNotesApp.swift
+//  SamNotes
+//
+//  Created by Silvanei  Martins on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SamNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
